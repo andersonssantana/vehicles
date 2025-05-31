d99dc00a88bfc34a28e7ec9068191676c0871ca9
Anderson Santana <anderslash@gmail.com>
2025-05-31 11:15:50 -0300

    Refactor vehicle insertion logic to load data from JSON file

--- server/main.js
+++ server/main.js
@@ -1,56 +1,25 @@
 import { Meteor } from 'meteor/meteor';
 import { VeiculosCollection } from '../imports/api/veiculos';
 
-async function insertVehicle({
-  veiculo,
-  nome_abreviado_ptbr,
-  categoria,
-  local,
-  imani_tech,
-  hsw,
-  arena,
-  armado,
-  caracteristicas_especiais,
-}) {
+async function insertVehicle(vehicleData) {
   await VeiculosCollection.insertAsync({
-    veiculo,
-    nome_abreviado_ptbr,
-    categoria,
-    local,
-    imani_tech,
-    hsw,
-    arena,
-    armado,
-    caracteristicas_especiais,
+    ...vehicleData,
     createdAt: new Date()
   });
 }
 
 Meteor.startup(async () => {
   if (await VeiculosCollection.find().countAsync() === 0) {
-    await insertVehicle({
-        veiculo: "Bravado Buffalo EVX",
-        nome_abreviado_ptbr: "Buffalo EVX",
-        categoria: "Muscle",
-        local: "Agency: Hawick",
-        imani_tech: true,
-        hsw: true,
-        arena: false,
-        armado: false,
-        características_especiais: ""
-    });
-
-    await insertVehicle({
-        veiculo: "Bravado Buffalo STX",
-        nome_abreviado_ptbr: "Buffalo STX",
-        categoria: "Muscle",
-        local: "Agency: Hawick",
-        imani_tech: true,
-        hsw: false,
-        arena: false,
-        armado: false,
-        características_especiais: "Metralhadoras"
-    });
+    Assets.getTextAsync('veiculos_gta.json', async (error, data) => {
+    if (error) {
+        console.error("Error loading veiculos_gta.json:", error);
+        return;
+      }
+      const vehiclesData = JSON.parse(data);
+      for (const vehicle of vehiclesData) {
+        await insertVehicle(vehicle);
+      }
+    });    
   }
 
   Meteor.publish("veiculos", function () {
