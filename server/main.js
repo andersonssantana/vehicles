12353fce64aca82f97c0886eed22089f7a7bf95f
Anderson Santana <anderslash@gmail.com>
2025-05-31 10:29:18 -0300

    Add vehicle insertion logic and initial vehicle data to the database

--- server/main.js
+++ server/main.js
@@ -1,7 +1,58 @@
 import { Meteor } from 'meteor/meteor';
 import { VeiculosCollection } from '../imports/api/veiculos';
 
+async function insertVehicle({
+  veiculo,
+  nome_abreviado_ptbr,
+  categoria,
+  local,
+  imani_tech,
+  hsw,
+  arena,
+  armado,
+  caracteristicas_especiais,
+}) {
+  await VeiculosCollection.insertAsync({
+    veiculo,
+    nome_abreviado_ptbr,
+    categoria,
+    local,
+    imani_tech,
+    hsw,
+    arena,
+    armado,
+    caracteristicas_especiais,
+    createdAt: new Date()
+  });
+}
+
 Meteor.startup(async () => {
+  if (await VeiculosCollection.find().countAsync() === 0) {
+    await insertVehicle({
+      veiculo: "Blista",
+      nome_abreviado_ptbr: "Blista",
+      categoria: "Compactos",
+      local: "Los Santos",
+      imani_tech: false,
+      hsw: false,
+      arena: false,
+      armado: false,
+      caracteristicas_especiais: "Nenhuma"
+    });
+
+    await insertVehicle({
+      veiculo: "Blista Compact",
+      nome_abreviado_ptbr: "Blista Compact",
+      categoria: "Compactos",
+      local: "Los Santos",
+      imani_tech: false,
+      hsw: true,
+      arena: false,
+      armado: false,
+      caracteristicas_especiais: "Nenhuma"
+    });
+  }
+
   Meteor.publish("veiculos", function () {
     return VeiculosCollection.find();
   });
